175580b4388a77aac3901e2ebcacac2983aa7264
unknown <mjgutierre@eafit.edu.co>
2023-10-04 16:22:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+const port = 80;
+
+const phrases = [
+  "Get ready to be inspired…",
+
+  "See rejection as redirection.",
+
+  "There is beauty in simplicity.",
+
+  "You can’t be late until you show up.",
+
+  "Maybe life is testing you. Don’t give up.",
+
+  "Impossible is just an opinion.",
+
+  "Alone or not you gonna walk forward.",
+];
+
+app.get("/", (req, res) => {
+  const number = Math.floor(Math.random() * 7);
+
+  res.send(phrases[number]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
