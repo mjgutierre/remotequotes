3450f6595f926a7034d102b1e0ec321c16ac86bf
unknown <mjgutierre@eafit.edu.co>
2023-10-18 07:54:34 -0500

    cambios tutorial07

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const express = require("express");
-
+const os = require("os");
 const app = express();
-
 const port = 80;
 
 const phrases = [
   "Get ready to be inspired…",
-
   "See rejection as redirection.",
-
   "There is beauty in simplicity.",
-
   "You can’t be late until you show up.",
-
   "Maybe life is testing you. Don’t give up.",
-
   "Impossible is just an opinion.",
-
   "Alone or not you gonna walk forward.",
 ];
 
 app.get("/", (req, res) => {
   const number = Math.floor(Math.random() * 7);
-
-  res.send(phrases[number]);
+  res.send(phrases[number] + " - Container Id: " + os.hostname());
 });
 
 app.listen(port, () => {
